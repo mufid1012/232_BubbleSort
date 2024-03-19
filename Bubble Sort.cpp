cf51a9e9167ccc11794e78238b3797f1f781b9f0
mufid1012 <mufidghibran1012@gmail.com>
2024-03-19 07:38:50 +0700

    Membuat Variabel Input

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-
 using namespace std;
 
 int arr[20];
 int n;
+
+void input(){
+    while (true)
+    {
+        cout <<"masukan banyaknya elemen array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 eleman.\n";
+        }
+    }
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "masukan elemen array" << endl;
+    cout << "====================" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "data ke-" << (i + 1 ) << ": ";
+        cin >> arr[i];
+    }
 
 int main(){
 
