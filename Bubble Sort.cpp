c6ae09bc262102c97157e465a80471a1477d1c64
mufid1012 <mufidghibran1012@gmail.com>
2024-03-19 07:39:30 +0700

    Membuat variabel bubblesort

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -29,3 +29,16 @@ int main(){
 
 }
 
+}void bubbleSortArray(){ //procedur untuk mengurutkan array
+    int pass= 1; //step 1
+    do {
+        for(int j = 0; j <= n - 1 - pass; j++){ //step 2
+            if(arr[j] > arr[j + 1]){ //step 3
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        pass = pass + 1; //step 4 
+    } while (pass <= n - 1); //step 5
+
